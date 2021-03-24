9d040c524c3cd2f70e2b1007d068f601399a7146
Ivan Osypov <oivanfs@gmail.com>
2021-03-24 13:03:28 +0200

    Added joins.sql

---
+++ joins.sql
@@ -0,0 +1,55 @@
+USE AdventureWorks2019
+GO
+
+SELECT p.FirstName
+	,p.LastName
+	,e.JobTitle
+	,e.BirthDate
+FROM Person.Person AS p
+INNER JOIN HumanResources.Employee AS e ON p.BusinessEntityID = e.BusinessEntityID;
+GO
+
+SELECT p.FirstName
+	,p.LastName
+	,(
+		SELECT e.JobTitle
+		FROM HumanResources.Employee AS e
+		WHERE p.BusinessEntityID = e.BusinessEntityID
+			AND e.JobTitle IS NOT NULL
+		) AS JobTitle
+FROM Person.Person AS p;
+GO
+
+SELECT t.FirstName
+	,t.LastName
+	,t.JobTitle
+FROM (
+	SELECT p.*
+		,e.JobTitle
+	FROM Person.Person AS p
+	INNER JOIN HumanResources.Employee AS e ON p.BusinessEntityID = e.BusinessEntityID
+	) t
+WHERE JobTitle IS NOT NULL;
+GO
+
+SELECT p.FirstName
+	,p.LastName
+	,e.JobTitle
+FROM Person.Person AS p
+CROSS JOIN HumanResources.Employee AS e;
+GO
+
+SELECT p.FirstName
+	,p.LastName
+	,e.JobTitle
+FROM Person.Person AS p
+CROSS JOIN HumanResources.Employee AS e;
+
+SELECT COUNT(*) AS Count
+FROM (
+	SELECT p.FirstName
+		,p.LastName
+		,e.JobTitle
+	FROM Person.Person AS p
+	CROSS JOIN HumanResources.Employee AS e
+	) t;
